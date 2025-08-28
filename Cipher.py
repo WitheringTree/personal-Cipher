4d68b0f6cf2383e7f08dbb4523e1faea4acb7c57
WitheringTree <164576270+WitheringTree@users.noreply.github.com>
2025-08-27 20:39:43 -0500

    Add files via upload

---
+++ Cipher.py
@@ -0,0 +1,71 @@
+import time
+import random
+class First:
+    comprehensive={}
+    first=[]
+    step=[]
+    lock=""
+    mess=""
+    folder=""
+
+    #lock=input("input KEY: ")
+
+    #This is a test string, should be removed after testing. Or rendered inert
+    lock="45plfao7vansepm7eifnpae4anva"
+
+    step=list(lock)
+    folder=step.pop
+
+    positions=0
+    
+    comprehensive=[[] for i in range(positions)]
+
+
+
+    mess=input("Place your message here: ")
+
+    for char in mess:
+            first.append(char)
+    if first != lock:
+         #Dud classification
+        print("7")
+    # 1
+    # 2
+    # 3
+
+
+
+    time.sleep(100)
+
+
+'''
+    question0=""    
+    answer1=0
+
+    while answer1==0:
+        question0=input("Encode or Decode? ")
+        if "1" == question0:
+            #encode
+            answer1=1  
+        elif "2" == question0:
+            #decode
+            answer1=2
+        else:
+            print("Not a valid answer")    
+
+            
+
+    length_Number_Characters=[]
+    wheel=[]
+    length_Bite_Size=[]    
+    distributor=[]
+    intermeditary=[]
+    
+    while count_Down>0:
+    distributor=lock.pop()
+    intermeditary=len(distributor)
+    length_Number_Characters.append(intermeditary)
+    wheel.append(distributor)
+    for char in distributor:
+        length_Bite_Size.append(char)
+'''
---
+++ Key.py
@@ -0,0 +1,66 @@
+import time
+import random
+class First:
+    initial=[]
+    wheel=[]
+    number_Of_Characters_List=[]
+    individual_Characters=[]
+    characters_Length=0
+    counter_Up=0
+    test=["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"]
+    key=""
+    question=""
+    question2=""
+    haver=""
+    
+    question=input("Do you want to edit a previous key? Y/N ")
+    if "y" in question:
+        question2=input("Please input held key: ")
+        print(question2)
+    else:
+        print("Continue as normal")
+        
+    number_Of_Positions=int(input("Specify number of positions: "))
+    static_Number=number_Of_Positions
+
+    time.sleep(0)
+    while number_Of_Positions>0:
+        counter_Up=1+counter_Up
+        number_Of_Positions=number_Of_Positions-1
+        print(counter_Up, " / ", static_Number)
+        characters=input("What do you want in the position: ")
+        characters_Length=len(characters)
+        number_Of_Characters_List.append(characters_Length)
+        wheel.append(characters)
+        for char in characters:
+            individual_Characters.append(char)
+        key=key+f'{characters_Length}{characters}'
+
+    key=f'{len(wheel)}'+key
+        
+    #this is to give the key maker something usable, so they don't have to keep inputing
+    print(key)
+
+
+    #individual_Characters=[*wheel]
+    #original_Wheel=["1il","26zr","3e","4a","5s","6g","7lt","8b","9jp","o0d","kcsx","f","h","m","n","u","v","w"]
+    #random_Integer=0
+
+    
+    #encodr=[i for i,x in enumerate(wheel)if x==a]
+
+    #Numbers in each position
+    print(number_Of_Characters_List)
+    #Intact
+    print(wheel)
+    #Broken into individual letters
+    print(individual_Characters)
+
+
+#numberofcharacterslist, 
+
+
+    #While ssem>0:
+    #    ssem==ab
+    
+    time.sleep(100)
